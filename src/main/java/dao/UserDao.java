b7cb7222868d650f3ad8ae8a938a6bc977484e2e
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-04-18 14:28:52 +0600

    Create findUserByEmailAndPassword() method.

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -4,4 +4,5 @@ import entity.User;
 
 public interface UserDao extends Dao<User> {
     void updateUserFull(User user) throws DaoException;
+    User findUserByEmailAndPassword(String email, String password) throws DaoException;
 }
