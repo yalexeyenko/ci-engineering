dad280145d56863eead567be0b2f77990067906b
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-04-18 13:22:31 +0600

    Create logger field.

--- src/main/java/servlet/ControllerServlet.java
+++ src/main/java/servlet/ControllerServlet.java
@@ -3,21 +3,19 @@ package servlet;
 import action.Action;
 import action.ActionFactory;
 import action.ActionResult;
-import connection.DBConnection;
-import entity.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 @WebServlet(name = "ControllerServlet", urlPatterns = "/do/*")
 public class ControllerServlet extends javax.servlet.http.HttpServlet {
+    private static final Logger log = LoggerFactory.getLogger(ControllerServlet.class);
+
     private ActionFactory actionFactory;
 
     @Override
@@ -27,10 +25,9 @@ public class ControllerServlet extends javax.servlet.http.HttpServlet {
 
     @Override
     public void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        log.debug("service()...");
         String actionName = req.getMethod() + req.getPathInfo();
         Action action = actionFactory.getAction(actionName);
-
-//        testConnection();
 
         if (action == null) {
             resp.sendError(HttpServletResponse.SC_NOT_FOUND, "Not found");
@@ -38,38 +35,17 @@ public class ControllerServlet extends javax.servlet.http.HttpServlet {
         }
 
         ActionResult result = action.execute(req, resp);
+        log.debug("result.getView(): {}", result.getView());
 
         if (result.isRedirect()) {
             String location = req.getContextPath() + "/do/" + result.getView();
+            log.debug("location: {}", location);
             resp.sendRedirect(location);
         } else {
             String path = "/WEB-INF/jsp/" + result.getView() + ".jsp";
+            log.debug("location: {}", path);
             req.getRequestDispatcher(path).forward(req, resp);
         }
 
     }
-
-//    public void testConnection() {
-//        System.out.println("!!!!!-1");
-//        DBConnection dbc = new DBConnection();
-//        System.out.println("!!!!!-2");
-//        Connection con = dbc.getConnection();
-//
-//        try {
-//            User user = new User();
-//            user.setFirstName("Kolya");
-//            user.setLastName("Ivanov");
-//            user.setEmail("ivalov@gmail.com");
-//            System.out.println("!!!!!-3");
-//            PreparedStatement statement = con.prepareStatement("insert into user (firstName, lastName, email) values (?, ?, ?)");
-//            System.out.println("!!!!!-4");
-//            statement.setString(1, user.getFirstName());
-//            statement.setString(2, user.getLastName());
-//            statement.setString(3, user.getEmail());
-//            statement.executeUpdate();
-//            System.out.println("!!!!!-5");
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//    }
 }
