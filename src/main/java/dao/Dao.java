eba6fcd4063b6d53e9db3aa8d822c2229bcb6a09
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-04-18 14:31:22 +0600

    Refactor.

--- src/main/java/dao/Dao.java
+++ src/main/java/dao/Dao.java
@@ -5,11 +5,11 @@ import entity.BaseEntity;
 import java.util.List;
 
 public interface Dao<T extends BaseEntity> {
-    T findById(int id) throws DaoException;
     T insert(T t) throws DaoException;
+    T findById(int id) throws DaoException;
+    List<T> findAll() throws DaoException;
     void update(T t) throws DaoException;
     boolean delete(int id) throws DaoException;
-    List<T> findAll() throws DaoException;
 
 
 }
