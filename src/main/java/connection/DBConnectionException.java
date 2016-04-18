26d693ab345958e623b6d6839c14c69f07016982
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-04-18 13:24:04 +0600

    Delete class.

--- src/main/java/connection/DBConnectionException.java
+++
@@ -1,9 +0,0 @@
-package connection;
-
-public class DBConnectionException extends RuntimeException {
-    public DBConnectionException(String message) {
-    }
-
-    public DBConnectionException(String message, Exception exception) {
-    }
-}
