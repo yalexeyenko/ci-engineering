301bc64347ebc55a5d63804529f294c9ed03db39
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-04-18 13:28:23 +0600

    Modifed execute() method.

--- src/main/java/action/SignInAction.java
+++ src/main/java/action/SignInAction.java
@@ -1,11 +1,44 @@
 package action;
 
+import entity.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import service.UserService;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class SignInAction implements Action {
+    private static final Logger log = LoggerFactory.getLogger(SignInAction.class);
+
+    private ActionResult home = new ActionResult("home", true);
+    private ActionResult loginAgain = new ActionResult("welcome");
+
     @Override
     public ActionResult execute(HttpServletRequest req, HttpServletResponse resp) {
-        return null;
+        String email = req.getParameter("email");
+        String password = req.getParameter("password");
+
+        User user = new User();
+        user.setEmail(email);
+        user.setPassword(password);
+
+        UserService userService = new UserService();
+        user = userService.findUserByCredentials(email, password);
+        log.debug("user == null: {}", user == null);
+        try {
+            userService.close();
+        } catch (Exception e) {
+            throw new ActionException("Failed to close service", e);
+        }
+
+        if (user != null) {
+            req.getSession().setAttribute("user", user);
+            return home;
+        } else {
+            req.setAttribute("signInError", "email or password incorrect");
+            return loginAgain;
+        }
+
     }
 }
