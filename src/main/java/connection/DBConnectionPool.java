b81e5d179dbd4574a6b530410a9ff446f99edbe7
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-04-18 13:25:24 +0600

    Modified DBConnectionPool class.

---
+++ src/main/java/connection/DBConnectionPool.java
@@ -0,0 +1,39 @@
+package connection;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DBConnectionPool {
+
+    public static Connection getConnection() {
+        try {
+            return InstanceHolder.getInstance().getConnection();
+        } catch (SQLException e) {
+            throw new DBConnectionPoolException("Failed to get connection", e);
+        }
+    }
+
+    private static class InstanceHolder {
+        private static final DataSource DATASOURCE = create();
+
+        private static DataSource create() {
+            InitialContext initialContext = null;
+            DataSource dataSource = null;
+            try {
+                initialContext = new InitialContext();
+                dataSource = (DataSource) initialContext.lookup("java:/comp/env/jdbc/ci-engineering");
+            } catch (NamingException e) {
+                throw new DBConnectionPoolException("InitialContext error", e);
+            }
+            return dataSource;
+        }
+
+        public static DataSource getInstance() {
+            return DATASOURCE;
+        }
+    }
+}
