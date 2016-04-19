bbca6d56fba8a054cebdea0b1b5f92339ce5b21c
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-04-19 16:32:16 +0600

    Modified execute() method (create input validation).

--- src/main/java/action/SignUpAction.java
+++ src/main/java/action/SignUpAction.java
@@ -4,15 +4,25 @@ import entity.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import service.UserService;
+import validator.Validator;
+import validator.Violation;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+import java.util.Set;
 
 public class SignUpAction implements Action {
     private static final Logger log = LoggerFactory.getLogger(SignUpAction.class);
 
+    private Validator formValidator;
+
     private ActionResult home = new ActionResult("home", true);
     private ActionResult singUpAgain = new ActionResult("welcome");
+
+    public SignUpAction() {
+        formValidator = new Validator();
+    }
 
     @Override
     public ActionResult execute(HttpServletRequest req, HttpServletResponse resp) {
@@ -21,10 +31,22 @@ public class SignUpAction implements Action {
         String lastName = req.getParameter("lastName");
         String email = req.getParameter("email");
         String password = req.getParameter("password");
-        log.debug("firstName: {}", firstName);
-        log.debug("lastName: {}", lastName);
-        log.debug("email: {}", email);
-        log.debug("password: {}", password);
+
+
+        Map<String, String[]> parameterMap = req.getParameterMap();
+
+
+
+        Set<Violation> violations = formValidator.validate(parameterMap);
+        log.debug("violations.size(): {}", violations.size());
+        if (!violations.isEmpty()) {
+            for (Violation violation : violations) {
+                log.debug("violation.getName(): {}", violation.getName());
+                log.debug("violation.getViolation(): {}", violation.getViolation());
+                req.setAttribute(violation.getName(), violation.getViolation());
+            }
+            return singUpAgain;
+        }
 
         User user = new User();
         user.setFirstName(firstName);
@@ -50,7 +72,7 @@ public class SignUpAction implements Action {
             return home;
         } else {
             log.debug("user == null. home.getView(): {}", home.getView());
-            req.setAttribute("signUpError", "email already exists");
+            req.setAttribute("signUpError", "Email already registered");
             return singUpAgain;
         }
     }
