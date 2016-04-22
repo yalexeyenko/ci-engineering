8d097f886727b911cc4648e573aa4e7cfca012b0
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-04-22 16:26:56 +0600

    Delete updateUserFull() method.

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -3,6 +3,5 @@ package dao;
 import entity.User;
 
 public interface UserDao extends Dao<User> {
-    void updateUserFull(User user) throws DaoException;
     User findUserByEmailAndPassword(String email, String password) throws DaoException;
 }
