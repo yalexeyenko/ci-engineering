d452be4d37eb8ca43ece7534aadc1dde2a78540b
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-04-23 23:24:51 +0600

    Create findAll(int, int) method.

--- src/main/java/dao/Dao.java
+++ src/main/java/dao/Dao.java
@@ -8,6 +8,7 @@ public interface Dao<T extends BaseEntity> {
     T insert(T t) throws DaoException;
     T findById(int id) throws DaoException;
     List<T> findAll() throws DaoException;
+    List<T> findAll(int start, int count) throws DaoException;
     void update(T t) throws DaoException;
     boolean delete(int id) throws DaoException;
 
