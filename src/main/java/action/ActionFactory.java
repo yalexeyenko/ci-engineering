7b222b1d8e0cfc0b13d7a2f878b5ae3535805c3b
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-04-24 23:44:52 +0600

    Put Actions to "actions" map.

--- src/main/java/action/ActionFactory.java
+++ src/main/java/action/ActionFactory.java
@@ -16,8 +16,11 @@ public class ActionFactory {
         actions.put("GET/admin-content-staff", new AdminContentStaffAction());
 
 
-        actions.put("GET/user-main", new ShowPageAction("user-main"));
         actions.put("GET/manager-main", new ShowPageAction("manager-main"));
+        actions.put("GET/manager-content-action", new ManagerContentAction());
+        actions.put("GET/manager-content-projects", new ManagerContentProjectsAction());
+
+        actions.put("GET/user-main", new ShowPageAction("user-main"));
         actions.put("GET/engineer-main", new ShowPageAction("engineer-main"));
         actions.put("GET/senior-main", new ShowPageAction("senior-main"));
 
@@ -27,6 +30,9 @@ public class ActionFactory {
 
         actions.put("POST/editProfile", new EditProfileAction());
         actions.put("GET/edit-profile", new ShowPageAction("edit-profile"));
+
+        actions.put("GET/create-project", new ShowPageAction("create-project"));
+        actions.put("POST/createProject", new CreateProjectAction());
     }
 
     public Action getAction(String actionName) {
