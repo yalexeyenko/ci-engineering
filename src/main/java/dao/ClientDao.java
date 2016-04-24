60dd7c04fb13e7693a24cef51ad8fe2266097a51
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-04-24 23:48:13 +0600

    Create insertIndivid(), updateIndivid() methods.

--- src/main/java/dao/ClientDao.java
+++ src/main/java/dao/ClientDao.java
@@ -3,4 +3,6 @@ package dao;
 import entity.Client;
 
 public interface ClientDao extends Dao<Client> {
+    Client insertIndivid(Client individualClient) throws DaoException;
+    void updateIndivid(Client individualClient) throws DaoException;
 }
