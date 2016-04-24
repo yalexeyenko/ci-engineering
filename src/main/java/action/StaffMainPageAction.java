2f932d84c02bd1ee8804ad967da85a88cb2c44cf
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-04-24 23:57:21 +0600

    Edit managerMain field.

--- src/main/java/action/StaffMainPageAction.java
+++ src/main/java/action/StaffMainPageAction.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServletResponse;
 public class StaffMainPageAction implements Action {
     private ActionResult userMain = new ActionResult("user-main", true);
     private ActionResult adminContentAction = new ActionResult("admin-content-action", true);
-    private ActionResult managerMain = new ActionResult("manager-main", true);
+    private ActionResult managerMain = new ActionResult("manager-content-action", true);
     private ActionResult seniorMain = new ActionResult("senior-main", true);
     private ActionResult engineerMain = new ActionResult("engineer-main", true);
 
