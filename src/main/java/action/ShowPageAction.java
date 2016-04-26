c978cbb38dd827f2c18a8dff79f6274a73bcc97a
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-04-26 16:29:07 +0600

    Create log.

--- src/main/java/action/ShowPageAction.java
+++ src/main/java/action/ShowPageAction.java
@@ -1,9 +1,14 @@
 package action;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class ShowPageAction implements Action {
+    private static final Logger log = LoggerFactory.getLogger(ShowPageAction.class);
+
     private ActionResult result;
 
     public ShowPageAction(String page) {
