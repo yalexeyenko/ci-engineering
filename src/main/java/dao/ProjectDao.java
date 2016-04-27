d2e188fbdd2a09b7938cba09f30c2e06edd41cf9
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-04-27 15:38:54 +0600

    Create updateProjectSenior() mehtod.

--- src/main/java/dao/ProjectDao.java
+++ src/main/java/dao/ProjectDao.java
@@ -4,4 +4,5 @@ import entity.Project;
 
 public interface ProjectDao extends Dao<Project> {
     void updateProjectClient(Project projectWithClient) throws DaoException;
+    void updateProjectSenior(Project projectWithSenior) throws DaoException;
 }
