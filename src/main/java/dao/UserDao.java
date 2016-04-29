b0a267008b36db184ee5af909cd81542ba420983
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-04-29 17:41:33 +0600

    Create updateMainProfileInfo(), updatePassord() method.

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -7,4 +7,6 @@ import java.util.List;
 public interface UserDao extends Dao<User> {
     User findUserByEmailAndPassword(String email, String password) throws DaoException;
     List<User> findAllSeniors() throws DaoException;
+    void updateMainProfileInfo(User user) throws DaoException;
+    void updatePassword(User user) throws DaoException;
 }
