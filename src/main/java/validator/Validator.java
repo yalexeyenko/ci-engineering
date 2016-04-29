8eeb2bf331cd0cd9f6a3aba6108071fa90c0329d
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-04-29 17:39:27 +0600

    Edit validate() method, rename to validateMainUserInfoInput(). Create validatePassword(), addViolation() method.

--- src/main/java/validator/Validator.java
+++ src/main/java/validator/Validator.java
@@ -19,34 +19,45 @@ public class Validator {
         regexMap.put("degree", "[A-Za-z 0-9]{3,60}$");
         regexMap.put("role", "[A-Za-z]{3,20}$");
         regexMap.put("email", "^([\\w-]+(?:\\.[\\w-]+)*)@((?:[\\w-]+\\.)*\\w[\\w-]{0,66})\\.([a-z]{2,6}(?:\\.[a-z]{2})?)$");
+
         regexMap.put("password", "^[A-Za-z0-9!@#$%^&*()_]{6,20}$");
-        regexMap.put("current_password", "^[A-Za-z0-9!@#$%^&*()_]{6,20}$");
+        regexMap.put("current-password", "^[A-Za-z0-9!@#$%^&*()_]{6,20}$");
         regexMap.put("repeatPassword", "^[A-Za-z0-9!@#$%^&*()_]{6,20}$");
     }
 
     public Validator() {
     }
 
-    public Set<Violation> validate(Map<String, String[]> parameterMap) {
-        log.debug("validate...");
-
+    public Set<Violation> validateMainUserInfoInput(Map<String, String[]> parameterMap) {
+        log.debug("validateMainUserInfoInput...");
         Set<Violation> violations = new HashSet<>();
         for (Map.Entry<String, String[]> entry : parameterMap.entrySet()) {
-
             String value = entry.getValue()[0];
             String key = entry.getKey();
             String regex = regexMap.get(key);
-
             log.debug("key: {}", key);
             log.debug("value: {}", value);
             log.debug("regex: {}", regex);
+            if (!value.matches(regex)) {
+                addViolation(key, violations);
+            }
+        }
+        return violations;
+    }
 
-//            log.debug("repeatPassword.equals(key): {}", "repeatPassword".equals(key));
-
+    public Set<Violation> validatePassword(Map<String, String[]> parameterMap) {
+        log.debug("validatePassword()...");
+        Set<Violation> violations = new HashSet<>();
+        for (Map.Entry<String, String[]> entry : parameterMap.entrySet()) {
+            String value = entry.getValue()[0];
+            String key = entry.getKey();
+            String regex = regexMap.get(key);
+            log.debug("key: {}", key);
+            log.debug("value: {}", value);
+            log.debug("regex: {}", regex);
             if (key.equals("repeatPassword")) {
                 log.debug("password: {}", parameterMap.get("password")[0]);
                 log.debug("repeatPassword: {}", value);
-
                 if (!(value.equals(parameterMap.get("password")[0]))) {
                     Violation violation = new Violation();
                     violation.setName(key + "Violation");
@@ -55,14 +66,17 @@ public class Validator {
                 }
                 continue;
             }
-
             if (!value.matches(regex)) {
-                Violation violation = new Violation();
-                violation.setName(key + "Violation");
-                violation.setViolation("Please specify a valid " + key);
-                violations.add(violation);
+                addViolation(key, violations);
             }
         }
         return violations;
+    }
+
+    public void addViolation(String key, Set<Violation> violations) {
+        Violation violation = new Violation();
+        violation.setName(key + "Violation");
+        violation.setViolation("Please specify a valid " + key);
+        violations.add(violation);
     }
 }
