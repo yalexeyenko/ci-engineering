27383ce7fa8bd8e17ab59c98138f789731a9ee14
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-04-30 18:06:23 +0600

    Create parameters validation.

--- src/main/java/action/CreateProjectAction.java
+++ src/main/java/action/CreateProjectAction.java
@@ -7,20 +7,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import service.ProjectService;
 import validator.Validator;
+import validator.Violation;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+import java.util.Set;
 
 public class CreateProjectAction implements Action {
     private static final Logger log = LoggerFactory.getLogger(CreateProjectAction.class);
 
-    private Validator projectValidator;
+    private Validator validator;
 
     private ActionResult mainPage = new ActionResult("main-page", true);
-    private ActionResult createProjectAgain = new ActionResult("createProject");
+    private ActionResult createProjectAgain = new ActionResult("create-project");
 
     public CreateProjectAction() {
-        projectValidator = new Validator();
+        validator = new Validator();
     }
 
     @Override
@@ -30,6 +33,18 @@ public class CreateProjectAction implements Action {
         String projectDeadline = req.getParameter("projectDeadline");
         log.debug("projectName: {}", projectName);
         log.debug("projectDeadline: {}", projectDeadline);
+
+        Map<String, String[]> parameterMap = req.getParameterMap();
+        Set<Violation> violations = validator.validateProjectCreation(parameterMap);
+
+        log.debug("violations.size(): {}", violations.size());
+        if (!violations.isEmpty()) {
+            for (Violation violation : violations) {
+                req.setAttribute(violation.getName(), violation.getViolation());
+            }
+            req.setAttribute("unsavedName", projectName);
+            return createProjectAgain;
+        }
 
         Project currentProject = new Project();
         currentProject.setName(projectName);
