f23b7d6b9415f40122015c99df3f685a9b8e560c
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-05-02 18:07:16 +0600

    Add parameters validation. Create getCountries() method.

--- src/main/java/action/EditClientAction.java
+++ src/main/java/action/EditClientAction.java
@@ -7,15 +7,27 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import service.ClientService;
 import service.ProjectService;
+import validator.Validator;
+import validator.Violation;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 
 public class EditClientAction implements Action {
     private static final Logger log = LoggerFactory.getLogger(EditClientAction.class);
 
-    private ActionResult editClientAgain = new ActionResult("editClientAction");
-    private ActionResult viewClient = new ActionResult("view-client");
+    private Validator validator;
+
+    private ActionResult editClientAgain = new ActionResult("edit-client");
+    private ActionResult editClientSuccess = new ActionResult("edit-client");
+
+    public EditClientAction() {
+        validator = new Validator();
+    }
 
     @Override
     public ActionResult execute(HttpServletRequest req, HttpServletResponse resp) {
@@ -31,6 +43,32 @@ public class EditClientAction implements Action {
         String clientType = req.getParameter("clientType");
         String clientId = req.getParameter("clientId");
 
+        String projectId = req.getParameter("projectId");
+
+        Map<String, String[]> parameterMap = req.getParameterMap();
+        Set<Violation> violations = validator.validateClientInfo(parameterMap);
+
+        log.debug("violations.size(): {}", violations.size());
+        if (!violations.isEmpty()) {
+            for (Violation violation : violations) {
+                req.setAttribute(violation.getName(), violation.getViolation());
+            }
+            req.setAttribute("nameFirstName", nameFirstName);
+            req.setAttribute("fullNameLastName", fullNameLastName);
+            req.setAttribute("clientEmail", clientEmail);
+            req.setAttribute("clientCountry", clientCountry);
+            req.setAttribute("clientCity", clientCity);
+            req.setAttribute("clientAddress", clientAddress);
+            req.setAttribute("clientTelephone", clientTelephone);
+            req.setAttribute("clientBankAccountNumber", clientBankAccountNumber);
+            req.setAttribute("clientEinSsn",clientEinSsn );
+            req.setAttribute("clientType", clientType);
+            req.setAttribute("projectId", projectId);
+            req.setAttribute("clientId", clientId);
+            req.setAttribute("countriesMap", getCountries());
+            return editClientAgain;
+        }
+
         Client client = new Client();
         client.setFirstName(nameFirstName);
         client.setLastName(fullNameLastName);
@@ -44,26 +82,45 @@ public class EditClientAction implements Action {
         client.setClientType(Client.ClientType.valueOf(clientType));
         client.setId(Integer.valueOf(clientId));
 
-        String projectId = req.getParameter("projectId");
-
-        ProjectService projectService = new ProjectService();
         ClientService clientService = new ClientService();
-
-        Project project;
 
         try {
             clientService.updateClient(client);
-            project = projectService.findProjectById(Integer.valueOf(projectId));
         } catch (DaoException e) {
+            log.debug("Failed to updateClient()");
             try {
-                projectService.close();
+                clientService.close();
             } catch (Exception ex) {
                 throw new ActionException("Failed to close service", ex);
             }
             req.setAttribute("editClientInfoError", "Verify your data. Cannot edit client.");
             return editClientAgain;
         }
-        req.setAttribute("project", project);
-        return viewClient;
+
+        req.setAttribute("nameFirstName", nameFirstName);
+        req.setAttribute("fullNameLastName", fullNameLastName);
+        req.setAttribute("clientEmail", clientEmail);
+        req.setAttribute("clientCountry", clientCountry);
+        req.setAttribute("clientCity", clientCity);
+        req.setAttribute("clientAddress", clientAddress);
+        req.setAttribute("clientTelephone", clientTelephone);
+        req.setAttribute("clientBankAccountNumber", clientBankAccountNumber);
+        req.setAttribute("clientEinSsn",clientEinSsn );
+        req.setAttribute("clientType", clientType);
+        req.setAttribute("projectId", projectId);
+        req.setAttribute("countriesMap", getCountries());
+        req.setAttribute("clientId", clientId);
+        req.setAttribute("clientEdited", "Changes have been saved successfully.");
+        return editClientSuccess;
+    }
+
+    private Map<String, String> getCountries() {
+        String[] locales = Locale.getISOCountries();
+        Map<String, String> countriesMap = new TreeMap<>();
+        for (String countryCode : locales) {
+            Locale obj = new Locale("", countryCode);
+            countriesMap.put(obj.getCountry(), obj.getDisplayCountry(Locale.US));
+        }
+        return countriesMap;
     }
 }
