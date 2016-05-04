b9154771799877571cbe7998dd2e6bcd9775e3f7
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-05-04 17:55:37 +0600

    Implement findAllPersonalProjects() method.

--- src/main/java/dao/ProjectDaoImpl.java
+++ src/main/java/dao/ProjectDaoImpl.java
@@ -14,9 +14,10 @@ import java.util.List;
 public class ProjectDaoImpl implements ProjectDao {
     private static final Logger log = LoggerFactory.getLogger(ProjectDaoImpl.class);
 
-    private static final String INSERT_PROJECT = "INSERT INTO project (startDate, projectName, deadLine, finished) VALUES (?, ?, ?, ?) ";
-    private static final String FIND_PROJECT_BY_ID = "SELECT startDate, deadline, projectName, id, finished, clientId, seniorId FROM project WHERE id = ?";
+    private static final String INSERT_PROJECT = "INSERT INTO project (startDate, projectName, deadLine, finished, staffId) VALUES (?, ?, ?, ?, ?) ";
+    private static final String FIND_PROJECT_BY_ID = "SELECT startDate, deadline, projectName, id, finished, clientId, seniorId, staffId FROM project WHERE id = ?";
     private static final String FIND_ALL_PROJECTS = "SELECT * FROM project";
+    private static final String FIND_ALL_PERSONAL_PROJECTS = "SELECT startDate, deadline, projectName, id, finished, clientId, seniorId, staffId FROM project WHERE staffId = ?";
     private static final String FIND_ALL_PROJECTS_LIMITED = "SELECT * FROM project LIMIT ? OFFSET ?";
     private static final String UPDATE_PROJECT = "UPDATE project SET projectName = ?, deadline = ?, finished = ? WHERE id = ?";
     private static final String DELETE_PROJECT_BY_ID = "DELETE FROM project WHERE id = ?";
@@ -40,6 +41,7 @@ public class ProjectDaoImpl implements ProjectDao {
             preparedStatement.setString(2, project.getName());
             preparedStatement.setDate(3, new java.sql.Date(project.getDeadline().toDate().getTime()));
             preparedStatement.setBoolean(4, project.isFinished());
+            preparedStatement.setInt(5, project.getManager().getId());
             preparedStatement.executeUpdate();
             ResultSet resultSet = preparedStatement.getGeneratedKeys();
             resultSet.next();
@@ -66,9 +68,11 @@ public class ProjectDaoImpl implements ProjectDao {
         Project project = new Project();
         Client client;
         User senior;
+        User manager;
         PreparedStatement preparedStatement = null;
         int clientId;
         int seniorId;
+        int staffId;
         try {
             preparedStatement = connection.prepareStatement(FIND_PROJECT_BY_ID);
             log.debug("preparedStatement is null: {}", preparedStatement == null);
@@ -89,6 +93,11 @@ public class ProjectDaoImpl implements ProjectDao {
                 seniorId = resultSet.getInt("seniorId");
                 senior = userDao.findById(seniorId);
                 project.setSenior(senior);
+            }
+            if (resultSet.getInt("staffId") > 0) {
+                staffId = resultSet.getInt("staffId");
+                manager = userDao.findById(staffId);
+                project.setManager(manager);
             }
             return project;
         } catch (SQLException e) {
@@ -111,8 +120,10 @@ public class ProjectDaoImpl implements ProjectDao {
         List<Project> projects = new ArrayList<>();
         Client client;
         User senior;
+        User manager;
         int clientId;
         int seniorId;
+        int managerId;
         Statement statement = null;
         try {
             statement = connection.createStatement();
@@ -129,10 +140,15 @@ public class ProjectDaoImpl implements ProjectDao {
                     client = clientDao.findById(clientId);
                     project.setClient(client);
                 }
-                if(resultSet.getInt("seniorId") > 0) {
+                if (resultSet.getInt("seniorId") > 0) {
                     seniorId = resultSet.getInt("seniorId");
                     senior = userDao.findById(seniorId);
                     project.setSenior(senior);
+                }
+                if (resultSet.getInt("staffId") > 0) {
+                    managerId = resultSet.getInt("staffId");
+                    manager = userDao.findById(managerId);
+                    project.setManager(manager);
                 }
                 projects.add(project);
             }
@@ -241,5 +257,61 @@ public class ProjectDaoImpl implements ProjectDao {
                 }
             }
         }
+    }
+
+    @Override
+    public List<Project> findAllPersonalProjects(int managerId) throws DaoException {
+        log.debug("findAllPersonalProjects()...");
+        ClientDao clientDao = new ClientDaoImpl(connection);
+        UserDao userDao = new UserDaoImpl(connection);
+        List<Project> projects = new ArrayList<>();
+        Client client;
+        User senior;
+        User manager;
+        int clientId;
+        int seniorId;
+        int staffId;
+        PreparedStatement preparedStatement = null;
+        try {
+            preparedStatement = connection.prepareStatement(FIND_ALL_PERSONAL_PROJECTS);
+            log.debug("preparedStatement is null: {}", preparedStatement == null);
+            preparedStatement.setInt(1, managerId);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                Project project = new Project();
+                project.setStartDate(new LocalDate(resultSet.getDate("startDate")));
+                project.setDeadline(new LocalDate(resultSet.getDate("deadline")));
+                project.setName(resultSet.getString("projectName"));
+                project.setId(resultSet.getInt("id"));
+                project.setFinished(resultSet.getBoolean("finished"));
+                if (resultSet.getInt("clientId") > 0) {
+                    clientId = resultSet.getInt("clientId");
+                    client = clientDao.findById(clientId);
+                    project.setClient(client);
+                }
+                if (resultSet.getInt("seniorId") > 0) {
+                    seniorId = resultSet.getInt("seniorId");
+                    senior = userDao.findById(seniorId);
+                    project.setSenior(senior);
+                }
+                if (resultSet.getInt("staffId") > 0) {
+                    staffId = resultSet.getInt("staffId");
+                    manager = userDao.findById(staffId);
+                    project.setManager(manager);
+                }
+                projects.add(project);
+            }
+        } catch (SQLException e) {
+            throw new DaoException("SQL FIND_PROJECT_BY_ID error.", e);
+        } finally {
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    throw new DaoException("Failed to close PreparedStatement", e);
+                }
+            }
+        }
+        return projects;
     }
 }
