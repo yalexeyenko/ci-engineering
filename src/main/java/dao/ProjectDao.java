cdc4d96ced8ca4759dd3463e2a04460349646ecb
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-05-04 17:55:01 +0600

    Creat findAllPersonalProjects() method.

--- src/main/java/dao/ProjectDao.java
+++ src/main/java/dao/ProjectDao.java
@@ -2,7 +2,10 @@ package dao;
 
 import entity.Project;
 
+import java.util.List;
+
 public interface ProjectDao extends Dao<Project> {
     void updateProjectClient(Project projectWithClient) throws DaoException;
     void updateProjectSenior(Project projectWithSenior) throws DaoException;
+    List<Project> findAllPersonalProjects(int managerId) throws DaoException;
 }
