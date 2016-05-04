b881c5f1207d176c906a70d8c1e4aac35d5e1cdf
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-05-04 11:28:06 +0600

    Add exceptions to execute() signature.

--- src/main/java/action/Action.java
+++ src/main/java/action/Action.java
@@ -1,8 +1,10 @@
 package action;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public interface Action {
-    ActionResult execute(HttpServletRequest req, HttpServletResponse resp);
+    ActionResult execute(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException;
 }
