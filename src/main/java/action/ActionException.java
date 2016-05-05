b373ff6aaaf8badda960453fdef8c046cf2b07ef
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-05-05 09:47:48 +0600

    Add constructor.

--- src/main/java/action/ActionException.java
+++ src/main/java/action/ActionException.java
@@ -4,4 +4,8 @@ public class ActionException extends RuntimeException {
     public ActionException(String message, Exception exception) {
         super(message, exception);
     }
+
+    public ActionException(String message) {
+        super(message);
+    }
 }
