b4f86bad09aae358d35bbbd23747eefef6c2b87b
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-05-05 09:48:17 +0600

    Create comments.

--- src/main/java/action/ActionFactory.java
+++ src/main/java/action/ActionFactory.java
@@ -14,53 +14,53 @@ public class ActionFactory {
     public ActionFactory() {
         this.actions = new HashMap<>();
 
-        actions.put("GET/welcome", new ShowPageAction("welcome"));
+        actions.put("GET/welcome", new ShowPageAction("welcome"));//UNREGISTERED
 
-        actions.put("GET/main-page", new StaffMainPageAction());
+        actions.put("GET/main-page", new StaffMainPageAction());//all but UNREGISTERED
 
-        actions.put("GET/admin-content-action", new AdminContentAction());
+        actions.put("GET/admin-content-action", new AdminContentAction());//ADMIN
+        actions.put("GET/admin-content-staff", new AdminContentStaffAction());//ADMIN
+        actions.put("GET/admin-view-user", new ShowPageAction("admin-view-user"));//ADMIN
+        actions.put("POST/change-role", new ChangeRoleAction());//ADMIN
 
-        actions.put("GET/admin-content-staff", new AdminContentStaffAction());
-        actions.put("GET/admin-view-user", new ShowPageAction("admin-view-user"));
-        actions.put("GET/pass-userId", new PassUserIdAction());
-        actions.put("POST/change-role", new ChangeRoleAction());
+        actions.put("GET/pass-userId", new PassUserIdAction());//all but UNREGISTERED
 
-        actions.put("GET/manager-content-projects", new ManagerContentProjectsAction());
-        actions.put("GET/manager-content-action", new ManagerContentAction());
+        actions.put("GET/manager-content-projects", new ManagerContentProjectsAction());//MANAGER
+        actions.put("GET/manager-content-action", new ManagerContentAction());//MANAGER
+        actions.put("GET/manager-main", new ShowPageAction("manager-main"));//MANAGER
 
-        actions.put("GET/manager-main", new ShowPageAction("manager-main"));
-        actions.put("GET/user-main", new ShowPageAction("user-main"));
-        actions.put("GET/engineer-main", new ShowPageAction("engineer-main"));
-        actions.put("GET/senior-main", new ShowPageAction("senior-main"));
+        actions.put("GET/user-main", new ShowPageAction("user-main"));//REGISTERED
+        actions.put("GET/engineer-main", new ShowPageAction("engineer-main"));//ENGINEER
+        actions.put("GET/senior-main", new ShowPageAction("senior-main"));//SENIOR
 
-        actions.put("POST/signIn", new SignInAction());
-        actions.put("POST/signUp", new SignUpAction());
-        actions.put("GET/signOut", new SignOutAction());
+        actions.put("POST/signIn", new SignInAction());//UNREGISTERED
+        actions.put("POST/signUp", new SignUpAction());//UNREGISTERED
+        actions.put("GET/signOut", new SignOutAction());//all but UNREGISTERED
 
-        actions.put("GET/view-profile", new ShowPageAction("view-profile"));
-        actions.put("GET/edit-profile", new ShowPageAction("edit-profile"));
-        actions.put("POST/edit-main-profile-info", new EditMainProfileInfoAction());
-        actions.put("POST/change-password", new ChangePasswordAction());
+        actions.put("GET/view-profile", new ShowPageAction("view-profile"));//all but UNREGISTERED
+        actions.put("GET/edit-profile", new ShowPageAction("edit-profile"));//all but UNREGISTERED
+        actions.put("POST/edit-main-profile-info", new EditMainProfileInfoAction());//all but UNREGISTERED
+        actions.put("POST/change-password", new ChangePasswordAction());//all but UNREGISTERED
 
-        actions.put("GET/create-project", new ShowPageAction("create-project"));
-        actions.put("POST/createProject", new CreateProjectAction());
-        actions.put("POST/upload", new UploadFileAction());
+        actions.put("GET/create-project", new ShowPageAction("create-project"));//MANAGER
+        actions.put("POST/createProject", new CreateProjectAction());//MANAGER
+        actions.put("POST/upload", new UploadFileAction());//MANAGER
 
-        actions.put("GET/pass-projectId", new PassProjectIdAction());
-        actions.put("GET/view-project", new ShowPageAction("view-project"));
-        actions.put("GET/edit-main-project-info", new ShowPageAction("edit-main-project-info"));
-        actions.put("GET/add-file", new ShowPageAction("add-file"));
-        actions.put("POST/edit-main-project-info-post", new EditMainProjectInfoAction());
+        actions.put("GET/pass-projectId", new PassProjectIdAction());//MANAGER
+        actions.put("GET/view-project", new ShowPageAction("view-project"));//MANAGER
+        actions.put("GET/edit-main-project-info", new ShowPageAction("edit-main-project-info"));//MANAGER
+        actions.put("GET/add-file", new ShowPageAction("add-file"));//MANAGER
+        actions.put("POST/edit-main-project-info-post", new EditMainProjectInfoAction());//MANAGER
 
-        actions.put("GET/create-client", new ShowPageAction("create-client"));
-        actions.put("POST/createClientAction", new CreateClientAction());
+        actions.put("GET/create-client", new ShowPageAction("create-client"));//MANAGER
+        actions.put("POST/createClientAction", new CreateClientAction());//MANAGER
 
-        actions.put("GET/view-client", new ShowPageAction("view-client"));
-        actions.put("GET/edit-client", new ShowPageAction("edit-client"));
-        actions.put("POST/editClientAction", new EditClientAction());
+        actions.put("GET/view-client", new ShowPageAction("view-client"));//MANAGER
+        actions.put("GET/edit-client", new ShowPageAction("edit-client"));//MANAGER
+        actions.put("POST/editClientAction", new EditClientAction());//MANAGER
 
-        actions.put("GET/specify-senior", new ShowPageAction("specify-senior"));
-        actions.put("POST/specifySeniorAction", new SpecifySeniorAction());
+        actions.put("GET/specify-senior", new ShowPageAction("specify-senior"));//MANAGER
+        actions.put("POST/specifySeniorAction", new SpecifySeniorAction());//MANAGER
     }
 
     public Action getAction(String actionName) {
