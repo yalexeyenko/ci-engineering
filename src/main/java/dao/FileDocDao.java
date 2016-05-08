4387bc42d8c06cc6b92a74580f352fd7063de642
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-05-08 19:10:40 +0600

    Create findAllFileDocsByProjectsId() metthod.

--- src/main/java/dao/FileDocDao.java
+++ src/main/java/dao/FileDocDao.java
@@ -2,5 +2,8 @@ package dao;
 
 import entity.FileDoc;
 
+import java.util.List;
+
 public interface FileDocDao extends Dao<FileDoc> {
+    List<FileDoc> findAllFileDocsByProjectId(int projectId);
 }
