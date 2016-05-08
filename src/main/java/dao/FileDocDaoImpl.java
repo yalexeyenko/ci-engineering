fcfd326278946e4c20fa0d12239f0d562f88990f
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-05-08 10:34:26 +0600

    Delete "throws DaoException" from methods signature.

--- src/main/java/dao/FileDocDaoImpl.java
+++ src/main/java/dao/FileDocDaoImpl.java
@@ -4,7 +4,6 @@ import entity.FileDoc;
 import org.joda.time.LocalDate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import service.ModuleService;
 import service.ProjectService;
 import service.UserService;
 
@@ -16,13 +15,12 @@ public class FileDocDaoImpl implements FileDocDao {
     private static final Logger log = LoggerFactory.getLogger(FileDocDaoImpl.class);
 
     private static final String INSERT_FILEDOC = "INSERT INTO filedoc (description, lastModified, status, staffId," +
-            " moduleId, projectId, fileContent) VALUES (?, ?, ?, ?, ?, ?, ?)";
-    private static final String FIND_FILDOC_BY_ID = "SELECT description, lastModified, status, staffId, moduleId, projectId, fileContent" +
+            "  projectId, fileContent, fileName) VALUES (?, ?, ?, ?, ?, ?, ?)";
+    private static final String FIND_FILDOC_BY_ID = "SELECT description, lastModified, status, staffId, projectId, fileContent, fileName" +
             " FROM filedoc WHERE id = ?";
     private static final String FIND_ALL_FILEDOCS = "SELECT * FROM filedoc";
-    private static final String FIND_ALL_FILEDOCS_LIMIT = "SELECT * FROM filedoc LIMIT ? OFFSET ?";
     private static final String UPDATE_FILEDOC = "UPDATE filedoc SET description = ?, lastModified = ?, status = ?, staffId = ?," +
-            " moduleId = ?, projectId = ?, fileContent = ? WHERE id = ?";
+            " projectId = ?, fileContent = ?, fileName = ? WHERE id = ?";
     private static final String DELETE_FILEDOC_BY_ID = "DELETE FROM filedoc WHERE id = ?";
 
     private final Connection connection;
@@ -32,7 +30,7 @@ public class FileDocDaoImpl implements FileDocDao {
     }
 
     @Override
-    public FileDoc insert(FileDoc fileDoc) throws DaoException {
+    public FileDoc insert(FileDoc fileDoc) {
         PreparedStatement preparedStatement = null;
 
         try {
@@ -45,13 +43,9 @@ public class FileDocDaoImpl implements FileDocDao {
             preparedStatement.setDate(2, new Date(fileDoc.getLastModified().toDate().getTime()));
             preparedStatement.setString(3, fileDoc.getStatus().name());
             preparedStatement.setInt(4, fileDoc.getUser().getId());
-            if (fileDoc.getModule() != null) {
-                preparedStatement.setInt(5, fileDoc.getModule().getId());
-            } else {
-                preparedStatement.setObject(5, null);
-            }
-            preparedStatement.setInt(6, fileDoc.getProject().getId());
-            preparedStatement.setBinaryStream(7, fileDoc.getFileContent());// change to byte array if necessary
+            preparedStatement.setInt(5, fileDoc.getProject().getId());
+            preparedStatement.setBinaryStream(6, fileDoc.getFileContent());
+            preparedStatement.setString(7, fileDoc.getName());
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -81,9 +75,8 @@ public class FileDocDaoImpl implements FileDocDao {
     }
 
     @Override
-    public FileDoc findById(int id) throws DaoException {
+    public FileDoc findById(int id) {
         UserService userService = new UserService();
-        ModuleService moduleService = new ModuleService();
         ProjectService projectService = new ProjectService();
         FileDoc fileDoc = new FileDoc();
         PreparedStatement preparedStatement = null;
@@ -97,14 +90,11 @@ public class FileDocDaoImpl implements FileDocDao {
             fileDoc.setLastModified(new LocalDate(resultSet.getDate("lastModified")));
             fileDoc.setStatus(FileDoc.Status.valueOf(resultSet.getString("status")));
             fileDoc.setUser(userService.findUserById(Integer.valueOf(resultSet.getString("staffId"))));
-            if (resultSet.getString("moduleId") != null) {
-                fileDoc.setModule(moduleService.findModuleById(Integer.valueOf(resultSet.getString("moduleId"))));
-            }
             fileDoc.setProject(projectService.findProjectById(Integer.valueOf(resultSet.getString("projectId"))));
             fileDoc.setFileContent(resultSet.getBinaryStream("fileContent"));
+            fileDoc.setName(resultSet.getString("fileName"));
             return fileDoc;
         } catch (SQLException e) {
-            log.debug("Failed to FIND_FILDOC_BY_ID");
             throw new DaoException("SQL FIND_FILDOC_BY_ID error.", e);
         } finally {
             if (preparedStatement != null) {
@@ -118,9 +108,8 @@ public class FileDocDaoImpl implements FileDocDao {
     }
 
     @Override
-    public List<FileDoc> findAll() throws DaoException {
+    public List<FileDoc> findAll() {
         UserService userService = new UserService();
-        ModuleService moduleService = new ModuleService();
         ProjectService projectService = new ProjectService();
         List<FileDoc> fileDocs = new ArrayList<>();
         Statement statement = null;
@@ -134,15 +123,11 @@ public class FileDocDaoImpl implements FileDocDao {
                 fileDoc.setLastModified(new LocalDate(resultSet.getDate("lastModified")));
                 fileDoc.setStatus(FileDoc.Status.valueOf(resultSet.getString("status")));
                 fileDoc.setUser(userService.findUserById(Integer.valueOf(resultSet.getString("staffId"))));
-                if (resultSet.getString("moduleId") != null) {
-                    fileDoc.setModule(moduleService.findModuleById(Integer.valueOf(resultSet.getString("moduleId"))));
-                }
                 fileDoc.setProject(projectService.findProjectById(Integer.valueOf(resultSet.getString("projectId"))));
                 fileDoc.setFileContent(resultSet.getBinaryStream("fileContent"));
                 fileDocs.add(fileDoc);
             }
         } catch (SQLException e) {
-            log.debug("Failed to FIND_ALL_FILEDOCS");
             throw new DaoException("SQL FIND_ALL_FILEDOCS error.", e);
         } finally {
             if (statement != null) {
@@ -157,12 +142,7 @@ public class FileDocDaoImpl implements FileDocDao {
     }
 
     @Override
-    public List<FileDoc> findAll(int start, int count) throws DaoException {
-        return null;
-    }
-
-    @Override
-    public void update(FileDoc fileDoc) throws DaoException {
+    public void update(FileDoc fileDoc) {
         PreparedStatement preparedStatement = null;
         try {
             preparedStatement = connection.prepareStatement(UPDATE_FILEDOC);
@@ -170,17 +150,12 @@ public class FileDocDaoImpl implements FileDocDao {
             preparedStatement.setDate(2, new java.sql.Date(fileDoc.getLastModified().toDate().getTime()));
             preparedStatement.setString(3, fileDoc.getStatus().name());
             preparedStatement.setInt(4, fileDoc.getUser().getId());
-            if (fileDoc.getModule() != null) {
-                preparedStatement.setInt(5, fileDoc.getModule().getId());
-            } else {
-                preparedStatement.setInt(5, new Integer(null));
-            }
-            preparedStatement.setInt(6, fileDoc.getProject().getId());
-            preparedStatement.setBinaryStream(7, fileDoc.getFileContent());
+            preparedStatement.setInt(5, fileDoc.getProject().getId());
+            preparedStatement.setBinaryStream(6, fileDoc.getFileContent());
+            preparedStatement.setString(7, fileDoc.getName());
             preparedStatement.setInt(8, fileDoc.getId());
             preparedStatement.executeUpdate();
         } catch (SQLException e) {
-            log.debug("Failed to UPDATE_FILEDOC");
             throw new DaoException("SQL UPDATE_FILEDOC error.", e);
         } finally {
             if (preparedStatement != null) {
@@ -194,14 +169,13 @@ public class FileDocDaoImpl implements FileDocDao {
     }
 
     @Override
-    public boolean delete(int id) throws DaoException {
+    public boolean delete(int id) {
         PreparedStatement preparedStatement = null;
         try {
             preparedStatement = connection.prepareStatement(DELETE_FILEDOC_BY_ID);
             preparedStatement.setInt(1, id);
             return (preparedStatement.executeUpdate() != 0);
         } catch (SQLException e) {
-            log.debug("Failed to DELETE_FILEDOC_BY_ID");
             throw new DaoException("SQL DELETE_FILEDOC_BY_ID error.", e);
         } finally {
             if (preparedStatement != null) {
