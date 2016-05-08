46f2e734ecfc307aa2f97a67d6549495dffe3962
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-05-08 10:37:16 +0600

    Delete "throws DaoException" from methods signature.

--- src/main/java/dao/ProjectDao.java
+++ src/main/java/dao/ProjectDao.java
@@ -5,7 +5,7 @@ import entity.Project;
 import java.util.List;
 
 public interface ProjectDao extends Dao<Project> {
-    void updateProjectClient(Project projectWithClient) throws DaoException;
-    void updateProjectSenior(Project projectWithSenior) throws DaoException;
-    List<Project> findAllPersonalProjects(int managerId) throws DaoException;
+    void updateProjectClient(Project projectWithClient);
+    void updateProjectSenior(Project projectWithSenior);
+    List<Project> findAllPersonalProjects(int managerId);
 }
