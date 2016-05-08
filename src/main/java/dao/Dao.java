929f24c1d0e6b8bc611de8ac3a01af74ccc32b5b
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-05-08 10:28:55 +0600

    Delete "throws DaoException" from methods signature.

--- src/main/java/dao/Dao.java
+++ src/main/java/dao/Dao.java
@@ -5,12 +5,9 @@ import entity.BaseEntity;
 import java.util.List;
 
 public interface Dao<T extends BaseEntity> {
-    T insert(T t) throws DaoException;
-    T findById(int id) throws DaoException;
-    List<T> findAll() throws DaoException;
-    List<T> findAll(int start, int count) throws DaoException;
-    void update(T t) throws DaoException;
-    boolean delete(int id) throws DaoException;
-
-
+    T insert(T t);
+    T findById(int id);
+    List<T> findAll();
+    void update(T t);
+    boolean delete(int id);
 }
