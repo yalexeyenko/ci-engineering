aae44943bc0989c9a2cf1264380e2f7bc592618c
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-05-10 15:07:40 +0600

    Create ModuleService class.

---
+++ src/main/java/service/ModuleService.java
@@ -0,0 +1,53 @@
+package service;
+
+import com.sun.org.apache.xpath.internal.operations.Mod;
+import dao.DaoFactory;
+import dao.ModuleDao;
+import entity.Module;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+public class ModuleService implements AutoCloseable {
+    private static final Logger log = LoggerFactory.getLogger(ModuleService.class);
+
+    private DaoFactory jdbcDaoFactory;
+    private ModuleDao moduleDao;
+
+    public ModuleService() {
+        jdbcDaoFactory = DaoFactory.newInstance(DaoFactory.JDBC);
+        moduleDao = (ModuleDao) jdbcDaoFactory.createDao(Module.class);
+    }
+
+    public Module createModule(Module module) {
+        return moduleDao.insert(module);
+    }
+
+    public Module findModuleById(int id) {
+        return moduleDao.findById(id);
+    }
+
+    public void updateModule(Module module) {
+        moduleDao.update(module);
+    }
+
+    public boolean deleteModuleById(int id) {
+        return moduleDao.delete(id);
+    }
+
+    public List<Module> findAllModules() {
+        return moduleDao.findAll();
+    }
+
+    public List<Module> findModulesByProjectId(int projectId) {
+        return moduleDao.findModulesByProjectId(projectId);
+    }
+
+    @Override
+    public void close() throws Exception {
+        if (jdbcDaoFactory != null) {
+            jdbcDaoFactory.close();
+        }
+    }
+}
