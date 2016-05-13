4e4bb817ab508b5e957febe4186143c4b133a125
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-05-13 22:18:41 +0600

    Create addEngineerToModule(), deleteEngineerFromModule() action.

--- src/main/java/dao/ModuleDao.java
+++ src/main/java/dao/ModuleDao.java
@@ -6,4 +6,6 @@ import java.util.List;
 
 public interface ModuleDao extends Dao<Module> {
     List<Module> findModulesByProjectId(int projectId);
+    void addEngineerToModule(int moduleId, int engineerId);
+    boolean deleteEngineerFromModule(int moduleId, int engineerId);
 }
