1822590c78994415b4b6d4f2c2b7584c8c0b92f6
Yevgeniy Alexeyenko <yalexeyenko@gmail.com>
2016-05-13 22:22:48 +0600

    Create findAllEngineers(), findEngineersByModuleId() methods.

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -7,7 +7,9 @@ import java.util.List;
 public interface UserDao extends Dao<User> {
     User findUserByEmailAndPassword(String email, String password);
     List<User> findAllSeniors();
+    List<User> findAllEngineers();
     void updateMainProfileInfo(User user);
     void updatePassword(User user);
     void updateRole(User user);
+    List<User> findEngineersByModuleId(int moduleId);
 }
